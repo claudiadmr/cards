a50339c2d32f7f6cf4a678f9a0950d39864768bc
Claudia Ribeiro <claudia.ribeiro@atlanse.pt>
2021-05-22 23:21:38 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  posts = [
+    {
+      imageUrl: 'assets/tree.jpeg',
+      title: 'Neat Tree',
+      username: 'nature',
+      content: 'I saw this neat tree today',
+    },
+    {
+      imageUrl: 'assets/mountain.jpeg',
+      title: 'Snowy Mountain',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain',
+    },
+    {
+      imageUrl: 'assets/biking.jpeg',
+      title: 'Mountain Biking',
+      username: 'biking12222',
+      content: 'I did some biking today',
+    },
+    {
+      imageUrl: 'assets/biking.jpeg',
+      title: 'Mountain Biking',
+      username: 'biking12222',
+      content: 'I did some biking today',
+    },
+  ];
+}
